5a95421fbd4212177e5c49590ce6d5b473094f76
hirszu <96964839+pillowified@users.noreply.github.com>
2024-12-17 09:34:24 +0100

    Add files via upload

--- restauracja.php
+++ restauracja.php
@@ -1,7 +1,15 @@
 <?php
     echo "Dodano rezerwację do bazy";
-    
     $db = mysqli_connect("localhost", "root", "", "baza");
-    $query = mysqli_query($db, "INSERT INTO rezerwacje (data_rez, liczba_osob, telefon) VALUES ($_POST['data'], $_POST['ludzie'], $_POST['telefon']);");
-    mysqli_close($db);
+
+    $stmt = $db->prepare("INSERT INTO rezerwacje (data_rez, liczba_osob, telefon) VALUES (?, ?, ?)");
+
+    $data = isset($_POST['data']) ? $_POST['data'] : '';
+    $ludzie = isset($_POST['ludzie']) ? $_POST['ludzie'] : 0;
+    $telefon = isset($_POST['telefon']) ? $_POST['telefon'] : '';
+
+    $stmt->bind_param("sis", $data, $ludzie, $telefon);
+
+    $stmt->close();
+    $db->close();
 ?>
